3879a69283395dc3910272186151fcb2a0ffad58
HydratedPig <1162183871@qq.com>
2024-07-31 19:18:48 +0800

    fix: incorrect narrowing

--- packages/shared/src/dispose.ts
+++ packages/shared/src/dispose.ts
@@ -9,12 +9,14 @@ export function isIterable<T = any>(val: any): val is Iterable<T> {
 export function dispose<T extends IDisposable>(disposable: T): T
 export function dispose<T extends IDisposable>(disposable: T | undefined): T | undefined
 export function dispose<T extends IDisposable>(disposable: Iterable<T>): Array<T>
-export function dispose<T extends IDisposable>(arg: T | Array<T> | undefined): any {
+export function dispose<T extends IDisposable>(arg: T | Array<T> | undefined | Iterable<T>): any {
   if (isIterable(arg)) {
-    return arg.map((a) => {
+    const res: T[] = []
+    for (const a of arg) {
       a?.dispose()
-      return a
-    })
+      res.push(a)
+    }
+    return res
   }
   else {
     arg?.dispose()
