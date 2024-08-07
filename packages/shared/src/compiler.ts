e402fbc76bd0d0b43b22a04e431bcef95c133c02
HydratedPig <1162183871@qq.com>
2024-08-07 18:52:56 +0800

    chore: add metarials

--- packages/shared/src/compiler.ts
+++ packages/shared/src/compiler.ts
@@ -1,10 +1,7 @@
-import { optimizeContent } from "./optimize"
+import { optimizeContent } from './optimize'
 
 export default class SpriteCompiler {
   symbols = new Map()
-  private constructor(public svgo: boolean = true) {
-
-  }
 
   static instance: SpriteCompiler
 
@@ -17,6 +14,6 @@ export default class SpriteCompiler {
 
   addSymbol(opt: { id: string, content: string, filePath: string }) {
     const { id, content, filePath } = opt
-    const optimized = optimizeContent(content, this.svgo)
+    const optimized = optimizeContent(content)
   }
 }
--- packages/shared/src/optimize.ts
+++ packages/shared/src/optimize.ts
@@ -1,26 +1,8 @@
-
 import { SVG, cleanupSVG, runSVGO } from '@iconify/tools'
-import { optimize } from 'svgo'
 
-export function optimizeContent(content: string, svgo = true) {
-  if (svgo) {
-    return optimize(content, {
-      multipass: false,
-      plugins: [
-        {
-          name: 'preset-default',
-          params: {
-            overrides: {
-              removeViewBox: false,
-            },
-          },
-        },
-      ],
-    })
-  } else {
-    const svg = new SVG(content)
-    cleanupSVG(svg)
-    runSVGO(svg)
-    return svg.toMinifiedString()
-  }
+export function optimizeContent(content: string) {
+  const svg = new SVG(content)
+  cleanupSVG(svg)
+  runSVGO(svg)
+  return svg.toMinifiedString()
 }
